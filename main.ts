265a721e6ab1f3533402055149437950fdea51f5
fcoandrefc <117944547+fcoandrefc@users.noreply.github.com>
2023-01-05 21:49:12 -0300

    Limpar tela ao final.

--- main.ts
+++ main.ts
@@ -2,15 +2,19 @@ let numero = 0
 let índice = 0
 input.onButtonPressed(Button.A, function () {
     numero = 0
+    basic.clearScreen()
     for (let numero = 0; numero <= 10; numero++) {
         basic.showNumber(numero)
     }
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     numero = 0
     índice = 10
+    basic.clearScreen()
     for (let numero = 0; numero <= 10; numero++) {
         basic.showNumber(índice)
         índice += -1
     }
+    basic.clearScreen()
 })
